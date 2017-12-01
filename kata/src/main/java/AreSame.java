32473e5ef74fb610f05fdd0dd0bce1ea8fd6eb5a
최두석/TL개발팀/TL <setin82@ticketlink.co.kr>
2017-12-01 19:15:46 +0900

    codewar

---
+++ kata/src/main/java/AreSame.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Created by NHNEnt on 2017-11-30.
+ */
+public class AreSame {
+	public static boolean comp2(int[] a, int[] b) {
+
+		if (a == null || b == null) {
+			return false;
+		}
+
+		Set<Integer> aCollect = Arrays.stream(a).boxed().collect(Collectors.toSet());
+		Set<Integer> bCollect = Arrays.stream(b).boxed().collect(Collectors.toSet());
+
+		System.out.println(" a  = ");
+		aCollect.forEach(integer -> System.out.print(integer + ", "));
+		System.out.println("\n b  = ");
+		bCollect.forEach(integer -> System.out.print(integer + ", "));
+
+		return aCollect.stream().allMatch(integer -> {
+											  int pow = (int) Math.pow(integer, 2);
+											  boolean has = bCollect.contains(pow);
+											  if (has) {
+												  bCollect.remove(pow);
+											  }
+											  return has;
+										  }
+										 );
+
+	}
+
+	public static boolean comp(int[] a, int[] b) {
+
+		if (a == null || b == null) {
+			return false;
+		}
+
+		Set<Integer> aCollect = new HashSet<>();
+		Set<Integer> bCollect = Arrays.stream(b).boxed().collect(Collectors.toSet());
+		for (int i : a) {
+			aCollect.add(Math.abs(i));
+		}
+
+		System.out.println(" a  = ");
+		aCollect.forEach(integer -> System.out.print(integer + ", "));
+
+		System.out.println("\n b  = ");
+		bCollect.forEach(integer -> System.out.print(integer + ", "));
+
+		boolean result = bCollect.stream().allMatch(integer -> {
+														int sqrt = (int) Math.sqrt(integer);
+														boolean has = aCollect.contains(sqrt);
+														if (has) {
+															aCollect.remove(sqrt);
+														}
+														return has;
+													}
+												   );
+		return result;
+
+	}
+}
---
+++ kata/src/main/java/SmallestIntegerFinder.java
@@ -0,0 +1,12 @@
+import java.util.stream.IntStream;
+
+/**
+ * Created by NHNEnt on 2017-12-01.
+ */
+public class SmallestIntegerFinder {
+	public static int findSmallestInt(int[] args) {
+
+		//return IntStream.of(args).sorted().findFirst().getAsInt();
+		return IntStream.of(args).min().getAsInt();
+	}
+}
