2951321ed00795921f1b510f6f16d0aa6d0e6549
dsChoi <setin82@gmail.com>
2019-03-11 23:59:43 +0900

    intellij project file recreate ,
    grading solved

--- kata/src/main/java/hackerrank/Grading.java
+++ kata/src/main/java/hackerrank/Grading.java
@@ -1,4 +1,64 @@
-package hackerrank.sort;
+package hackerrank;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Grading {
+    /*
+     * Complete the gradingStudents function below.
+     */
+    static int[] gradingStudents(int[] grades) {
+        return Arrays.stream(grades).boxed().mapToInt(
+
+                integer -> {
+
+                    if(integer < 35){
+                        return integer;
+                    }
+
+                    int mock = integer / 5;
+                    int n = integer % 5;
+
+                    if (n >= 3) {
+                        return (mock + 1) * 5;
+                    } else {
+                        return integer;
+                    }
+
+
+                }
+        ).toArray();
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(scan.nextLine().trim());
+
+        int[] grades = new int[n];
+
+        for (int gradesItr = 0; gradesItr < n; gradesItr++) {
+            int gradesItem = Integer.parseInt(scan.nextLine().trim());
+            grades[gradesItr] = gradesItem;
+        }
+
+        int[] result = gradingStudents(grades);
+
+        for (int resultItr = 0; resultItr < result.length; resultItr++) {
+            bw.write(String.valueOf(result[resultItr]));
+
+            if (resultItr != result.length - 1) {
+                bw.write("\n");
+            }
+        }
+//
+        bw.newLine();
+
+        bw.close();
+    }
 }
